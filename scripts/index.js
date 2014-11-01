88226173f7da825b6f38d5b100712085505a8585
Wzy <wzywangzhiyong@gmail.com>
2014-11-02 02:45:42 +0800

    [wizcabbit]Add basic jekyllrb

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var $container = $("#proj-wrapper");
-    imagesLoaded(container, function() {
-      $container.masonry({
-        itemSelector: ".proj",
-      });
-    });
-});
