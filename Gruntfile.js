ac519507a9167adbd3dcfd9482ef7a3ca65150e8
Wzy <wzywangzhiyong@gmail.com>
2014-11-23 20:32:33 +0800

    [blog.cabbit.me]Add grunt auto delivery

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: [
+        '_site/*'
+      ]
+    },
+    uglify: {
+      js: {
+        files: {
+          '_site/all.min.js': [
+            'lib/jquery/jquery.min.js',
+            'lib/Han/han.min.js'
+          ]
+        }
+      }
+    },
+    cssmin: {
+      css: {
+        files: {
+          '_site/all.min.css': [
+            'css/common.css',
+            'css/index.css',
+            'css/post.css',
+            'lib/Han/han.min.css',
+            'lib/prism/prism.css'
+          ]
+        }
+      }
+    },
+    jekyll: {
+      dest: "./_site"
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jekyll');
+
+  grunt.registerTask('default', ['clean', 'jekyll', 'uglify', 'cssmin']);
+};
